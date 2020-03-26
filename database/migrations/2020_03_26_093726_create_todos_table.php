032c884889dcd5f40678776f3c50b1ea5c82ce77
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-03-26 10:47:53 +0100

    Add todos table migrations

---
+++ database/migrations/2020_03_26_093726_create_todos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->text('details')->nullable();
+            $table->date('dueDate')->nullable();
+            $table->dateTime('dateTimeDone')->nullable();
+            $table->dateTime('reminderDateTime')->nullable();
+            $table->timestamps();
+
+            // Table options
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
