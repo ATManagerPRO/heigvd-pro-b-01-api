deb32da7e9480b6baccb942002fe9f4cfc718a3a
Robin <sikiewz@gmail.com>
2020-03-28 19:50:05 +0100

    Added tags table migration + model with relations

---
+++ database/migrations/2020_03_26_091148_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('label');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            // Table options
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
