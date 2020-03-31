9f88c0f590533af96b9cbbfde1fc782354193aa7
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-03-31 17:47:44 +0200

    Users and folders seeds OK

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Folder;
-use App\Todo;
-use App\TodoList;
-use App\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
-
-    private $USER_NB = 5;
-
     /**
      * Seed the application's database.
      *
@@ -20,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(FoldersTableSeeder::class);
     }
 }
