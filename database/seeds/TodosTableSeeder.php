2d08b076eefb77f914d03196dc2746f6d5f29fcb
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-03-31 18:05:45 +0200

    prepare todos seed (wip)

---
+++ database/seeds/TodosTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        DB::table('todos')->insert([
+            'id' => 1,
+            'todoList_id' => 3,
+            'user_id' => null,
+            'title' =>,
+            'details' =>,
+            'dueDate' =>,
+            'dateTimeDone' =>
+            'reminderDateTime' =>
+            ''
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        */
+    }
+}
