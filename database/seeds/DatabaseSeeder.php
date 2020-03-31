90aaa47d6842a7a32ddd367928e60b0cf2eefc85
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-03-31 17:43:50 +0200

    Revert "Add folders seeds"
    This reverts commit b28c2a9069a4ec53d9a67c2528fa88aedab6a526.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,34 +19,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Create 5 users
-        $users = factory(User::class, $this->USER_NB)->create();
-
-        // For each user
-        $users->each(function($user){
-
-            // Create 1 folder
-            $folders = factory(Folder::class, 1)->create([
-                'user_id' => $user->id
-            ]);
-
-            // Create 1 todolist
-            $todoLists = factory(TodoList::class, 1)->create([
-                'user_id' => $user->id,
-                'folder_id' => $folders[0]->id
-            ]);
-
-            // Associate the todoList to the user
-            DB::table('todoList_user')->insert(
-                [
-                    'todoList_id' => $todoLists[0]->id,
-                    'user_id' => $user->id,
-                    'permissionLevel' => 0,
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]
-            );
-
-        });
+        $this->call(UsersTableSeeder::class);
     }
 }
