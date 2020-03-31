d7231b0d9d9095aa1b14b96a3ce7b95e59ea2363
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-03-31 17:05:33 +0200

    Refactor seed to use factory (user and folder)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Folder;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
+    private $USER_NB = 5;
+    private $FOLDER_NB = 1;
+
     /**
      * Seed the application's database.
      *
@@ -11,7 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(FoldersTableSeeder::class);
+        $users = factory(User::class, $this->USER_NB)->create();
+        $users->each(function($user){
+            factory(Folder::class, $this->FOLDER_NB)->create(['user_id' => $user->id]);
+        });
     }
 }
