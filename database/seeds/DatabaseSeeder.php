729f79956bb3d53e3a322bb4bb614f151190cdb2
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-04-01 11:49:06 +0200

    Add goalSeeder and update goalTableMigration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(IntervalTableSeeder::class);
         $this->call(TodosTableSeeder::class);
         $this->call(Tag_TodoTableSeeder::class);
+        $this->call(GoalTableSeeder::class);
     }
 }
