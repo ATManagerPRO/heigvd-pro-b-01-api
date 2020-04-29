7c150c95a5478306ba6dd7607f29808a7d1e78c8
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-04-29 12:14:29 +0200

    Add google sign-in page for web test

---
+++ resources/views/googleAuth/login.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Google sign-in</title>
+
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="519635922975-ith46aj5ntcff6ca7mt4130dk5ocrf1i.apps.googleusercontent.com">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+</head>
+<body>
+
+    <h1>Google sign-in</h1>
+
+    <p>
+        {{ $test  }}
+    </p>
+
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+
+    <script>
+
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        }
+
+    </script>
+
+</body>
+</html>
