6f399051b4fce6eb0e8c45f349f33df51bff3ccd
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-04-29 12:46:45 +0200

    Add send token to backend func.

--- resources/views/googleAuth/login.blade.php
+++ resources/views/googleAuth/login.blade.php
@@ -32,6 +32,18 @@
             console.log('Name: ' + profile.getName());
             console.log('Image URL: ' + profile.getImageUrl());
             console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            console.log('Token : ' + googleUser.getAuthResponse().id_token);
+
+            // Send token to backend
+            /*
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = function() {
+                console.log('Signed in as: ' + xhr.responseText);
+            };
+            xhr.send('idtoken=' + id_token);
+            */
         }
 
     </script>
