ea58a42ecd61f7bfc54d2f9358bb84742b54bbaf
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-05-14 11:31:51 +0200

    Update DB users and add current test user to chaukot.dev@gmal.com

--- database/seeds/TodosTableSeeder.php
+++ database/seeds/TodosTableSeeder.php
@@ -16,7 +16,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 1,
             'todo_list_id' => 3,
-            'user_id' => 1, // Whose assigned
             'interval_id' => null,
             'title' => 'Test des battle royal disponibles',
             'details' => 'Tester tous les battle royal disponibles et faire un classement du pire au meilleur.',
@@ -33,7 +32,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 2,
             'todo_list_id' => 3,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Préparer live twitch',
             'details' => 'Préparer tout ce qu il faut pour setup un live twitch.',
@@ -50,7 +48,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 3,
             'todo_list_id' => 2,
-            'user_id' => null, // Whose assigned
             'interval_id' => 2,
             'title' => 'Jour des jambes',
             'details' => 'Barbell squat, Leg press, Leg extensions',
@@ -67,7 +64,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 4,
             'todo_list_id' => 1,
-            'user_id' => null, // Whose assigned
             'interval_id' => 1,
             'title' => 'Devoir',
             'details' => 'Faire mes devoirs comme le bon élève que je suis',
@@ -84,7 +80,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 5,
             'todo_list_id' => 4,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Faire les cours',
             'details' => 'Prendre du lait et du chocolat',
@@ -104,7 +99,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 6,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => 3,
             'title' => 'Payer internet',
             'details' => 'Payer facture mensuel d\'internet',
@@ -121,7 +115,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 7,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => 3,
             'title' => 'Payer swisscom natel',
             'details' => 'Payer facture mensuel de swisscom pour le natel',
@@ -138,7 +131,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 8,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Payer trajet CFF',
             'details' => 'Payer facture trajets CFF si y en a',
@@ -155,7 +147,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 9,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Payer casque HS70 digitec',
             'details' => 'Payer facture du casque gaming corsair HS70 PRO',
@@ -172,7 +163,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 10,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Payer armoire IKEA',
             'details' => 'Payer facture armoire murale IKEA',
@@ -189,7 +179,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 11,
             'todo_list_id' => 5,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Payer TV digitec',
             'details' => 'Payer TV samsung digitec',
@@ -206,7 +195,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 12,
             'todo_list_id' => 6,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Meuble à prendre',
             'details' => 'meuble salle de bain, TV, meuble TV, canapé, tout le cheni sur le bureau, ustensiles de cuisine, figurines POP, tout ce qu\'il y a sous le lit, guitares + batterie, alcool + meuble alcool, tous les trucs du chat, tableaux + posters.',
@@ -223,7 +211,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 13,
             'todo_list_id' => 6,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Chose à trier (goodies & habits)',
             'details' => 'Trier goodies bureau et tous les habits',
@@ -240,7 +227,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 14,
             'todo_list_id' => 6,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Plan nouvel appart',
             'details' => 'Faire les plans sur ordi de l\'appart et voir qu\'est ce qui passe ou comme meuble.',
@@ -257,7 +243,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 15,
             'todo_list_id' => 6,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Prendre mesures',
             'details' => 'Prendre les mesures du nouvel appart (pas oublié le double mètre)',
@@ -274,7 +259,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 16,
             'todo_list_id' => 7,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Rendu labo3 River',
             'details' => 'Rendu labo3 River',
@@ -291,7 +275,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 17,
             'todo_list_id' => 7,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Exo slide 12',
             'details' => 'Faire exo slide 12 (attention, sans dynamic ast !)',
@@ -308,7 +291,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 18,
             'todo_list_id' => 7,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Exo slide 24',
             'details' => 'Faire exo slide 24, possible en groupe',
@@ -325,7 +307,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 19,
             'todo_list_id' => 7,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'EXAMEN',
             'details' => 'Examen salle B02',
@@ -342,7 +323,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 20,
             'todo_list_id' => 8,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Labo5 : TD',
             'details' => 'Finir et rendre labo5 TDD',
@@ -359,7 +339,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 21,
             'todo_list_id' => 8,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Ex UML',
             'details' => 'Faire en solo l\'exo UML',
@@ -376,7 +355,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 22,
             'todo_list_id' => 8,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'EXAMEN',
             'details' => 'Examen salle B01',
@@ -393,7 +371,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 23,
             'todo_list_id' => 9,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Pâté en croûte fête famille',
             'details' => 'Préparer le filet de porc et la pâte feuilleté, pour pouvoir amener lundi en Valais',
@@ -410,7 +387,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 24,
             'todo_list_id' => 9,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Faire croissants au jambon',
             'details' => 'Préparer les croissants au jambon pour l\'anni à John',
@@ -427,7 +403,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 25,
             'todo_list_id' => 11,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'A prendre pour la plage',
             'details' => 'Maillot de bain, crème solaire, chaise longue, parasol, sceau, lunette + tuba, kayak, bouillée.',
@@ -444,7 +419,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 26,
             'todo_list_id' => 11,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'A prendre pour la montagne',
             'details' => 'Habits chaud, gourde, chaussure montagnem, crème solaire, sac, tente, lampe frontale, à manger.',
@@ -461,7 +435,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 27,
             'todo_list_id' => 12,
-            'user_id' => 1, // Whose assigned
             'interval_id' => null,
             'title' => 'Billet avion',
             'details' => 'Penser à réserver les billets d\'avion',
@@ -478,7 +451,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 28,
             'todo_list_id' => 12,
-            'user_id' => 1, // Whose assigned
             'interval_id' => null,
             'title' => 'Chambre d\'hôtel',
             'details' => 'Penser à réserver l\'hôtel',
@@ -495,7 +467,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 29,
             'todo_list_id' => 13,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Liste de course',
             'details' => 'Pain, céréales, bonbons, steak wapiti, purée, broccolis, banane, moutarde, langue de boeuf',
@@ -512,7 +483,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 30,
             'todo_list_id' => 13,
-            'user_id' => 1, // Whose assigned
             'interval_id' => null,
             'title' => 'Course pour la soirée potes',
             'details' => 'Olives, chips, bières, bières et bières',
@@ -529,7 +499,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 31,
             'todo_list_id' => 14,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Sorties de jeux cool',
             'details' => 'WoW nouvelle extension (juillet), valorant (juillet), assassin\'s creed (2021)',
@@ -546,7 +515,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 32,
             'todo_list_id' => 15,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Amener réparer la Gibson',
             'details' => 'Amener à Marclay musique la Gibson ',
@@ -563,7 +531,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 33,
             'todo_list_id' => 15,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Aller rechercher la Gibson',
             'details' => 'Aller rechercher à Marclay musique la Gibson',
@@ -580,7 +547,6 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->insert([
             'id' => 34,
             'todo_list_id' => 15,
-            'user_id' => null, // Whose assigned
             'interval_id' => null,
             'title' => 'Liste chansons à apprendre',
             'details' => 'Deep Purple (smoke on the water), Metallica (nothing else matter), Sum41 (over my head)',
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'id' => 1,
-            'googleId' => Str::random(50),
+            'googleId' => Str::random(21),
             'email' => 'gollgot@gmail.com',
             'tokenAPI' => Str::random(env("AUTH_TOKEN_LENGTH")),
             'created_at' => now(),
@@ -24,7 +24,7 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             'id' => 2,
-            'googleId' => Str::random(50),
+            'googleId' => Str::random(21),
             'email' => 'sikiewz@gmail',
             'tokenAPI' => Str::random(env("AUTH_TOKEN_LENGTH")),
             'created_at' => now(),
@@ -33,7 +33,7 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             'id' => 3,
-            'googleId' => Str::random(50),
+            'googleId' => Str::random(21),
             'email' => 'chucknorris@gmail.com',
             'tokenAPI' => Str::random(env("AUTH_TOKEN_LENGTH")),
             'created_at' => now(),
@@ -42,9 +42,9 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             'id' => 4,
-            'googleId' => Str::random(50),
-            'email' => 'alex@gmail.com',
-            'tokenAPI' => Str::random(env("AUTH_TOKEN_LENGTH")),
+            'googleId' => '111110858215912685956',
+            'email' => 'chaukot.dev@gmail.com',
+            'tokenAPI' => '4a420c1b49e0e3352ef8f0de6da1249f',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
